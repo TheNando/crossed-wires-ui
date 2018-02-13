830e70adff6f6e08935972ffc9c67aa9dd112ee5
Nando Pena <fernando.pena@rackspace.com>
2018-02-12 20:49:25 -0600

    feat(StatusBar): begin moving timer to status bar; end of UI 1.0

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -5,16 +5,8 @@ import MenuBar from 'components/MenuBar'
 import { Firebase } from 'services/Data'
 import './QuizPage.css'
 
-const DURATION = (20 * 1000)
 
 class QuizPage {
-    updateTimer () {
-        const timeLeft = this.question.expires - Date.now()
-        const percent =  (timeLeft / DURATION) * 100
-        this.timerElement.style.width = `${percent}%`
-        this.stopTimer = window.requestAnimationFrame(this.updateTimer.bind(this))
-    }
-
     oninit () {
         this.question = {
             answer: '',
@@ -29,15 +21,10 @@ class QuizPage {
             .onSnapshot(doc => (this.question = doc.data()) && m.redraw())
     }
 
-    oncreate (vnode) {
-        this.timerElement = vnode.dom.querySelector('.count-down')
-        this.updateTimer()
-    }
 
     // suspend real-time updates when page is not in view
     onremove () {
         this.pause()
-        window.cancelAnimationFrame(this.stopTimer)
     }
 
     view () {
@@ -47,7 +34,6 @@ class QuizPage {
                 m('.question-container', [
                     m('.text', this.question.text),
                 ]),
-                m('.count-down'),
                 this.question.choices.map(item => m('.choice-container', [
                     m('.text', item),
                 ])),
--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -2,12 +2,31 @@ import m from 'mithril'
 
 import './StatusBar.css'
 
+const DURATION = (20 * 1000)
 
 class StatusBar {
+    oncreate (vnode) {
+        this.timerElement = vnode.dom.querySelector('.count-down')
+        this.updateTimer()
+    }
+
+    updateTimer () {
+        // const timeLeft = this.question.expires - Date.now()
+        const timeLeft = Date.now()
+        const percent =  (timeLeft / DURATION) * 100
+        this.timerElement.style.width = `${percent}%`
+        this.stopTimer = window.requestAnimationFrame(this.updateTimer.bind(this))
+    }
+
     view () {
         return m('status', [
-            m('.grow', 'Crossed Wires'),
-            m('div', 'O'),
+            m('.bar', [
+                m('.grow', 'Crossed Wires'),
+                m('.avatar', [
+                    m('.badge', '50'),
+                ]),
+            ]),
+            m('.count-down'),
         ])
     }
 }
